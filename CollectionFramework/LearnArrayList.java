ec815d6b32ca557389793cd943a2c74c44588778
Vimal Kumar <vimal.kumar@imzcorporate.com>
2024-10-03 12:05:51 +0530

    Initiated collection Framework.

---
+++ CollectionFramework/LearnArrayList.java
@@ -0,0 +1,8 @@
+package CollectionFramework;
+import java.util.ArrayList;
+public class LearnArrayList {
+    public static void main(String[] args){
+        ArrayList<Integer> list=new ArrayList<>();
+
+    }
+}
