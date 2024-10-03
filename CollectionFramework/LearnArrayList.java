7228468fce31b0e93a7d6e5a573c5a4a1e3f4c22
vimal kumar <kvimal9560@gmail.com>
2024-10-04 00:17:57 +0530

    learn array list.

--- CollectionFramework/LearnArrayList.java
+++ CollectionFramework/LearnArrayList.java
@@ -4,5 +4,17 @@ public class LearnArrayList {
     public static void main(String[] args){
         ArrayList<Integer> list=new ArrayList<>();
 
+        System.out.println(list.add(1));
+        System.out.println(list.add(2));
+        System.out.println(list.add(3));
+        System.out.println(list);
+        System.out.println(list.add(4));
+        System.out.println(list.add(5));
+        System.out.println(list.get(2));
+        System.out.println(list.remove(3));
+        System.out.println(list);
+        System.out.println(list.set(3,9));
+        System.out.println(list);
+
     }
 }
