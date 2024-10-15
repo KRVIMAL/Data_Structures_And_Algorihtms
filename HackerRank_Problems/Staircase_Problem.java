f76b2ded1e29738d20086f7dedb6209c7140505a
Vimal Kumar <vimal.kumar@imzcorporate.com>
2024-10-15 16:10:35 +0530

    solved staircase problem.

---
+++ HackerRank_Problems/Staircase_Problem.java
@@ -0,0 +1,24 @@
+package HackerRank_Problems;
+
+import java.util.Scanner;
+
+public class Staircase_Problem {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int  n=sc.nextInt();
+        staircase(n);
+
+    }
+    public static void staircase(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<i+1;j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+    }
+}
