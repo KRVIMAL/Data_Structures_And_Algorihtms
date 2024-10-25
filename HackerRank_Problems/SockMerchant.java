04c4a8570356116920458a510a99ecd352bb3039
Vimal Kumar <kvimal9560@gmail.com>
2024-10-25 15:51:09 +0530

    solved SockMerchant Problem

--- HackerRank_Problems/SockMerchant.java
+++ HackerRank_Problems/SockMerchant.java
@@ -1,9 +1,10 @@
-package  HackerRank_Problems;
-import java.util.Scanner;
+package HackerRank_Problems;
+
 import java.util.Map;
+import java.util.Scanner;
 import java.util.HashMap;
 
-public class MigratoryBirds{
+public class SockMerchant{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -12,29 +13,24 @@ public class MigratoryBirds{
             arr[i]=sc.nextInt();
         }
         Map<Integer,Integer>hash=new HashMap<>();
-        int result=migratoryBirds(arr,hash);
+        int result=sockMerchant(arr, hash);
         System.out.println(result);
         sc.close();
     }
-    public static int migratoryBirds(int[] arr,Map<Integer,Integer>hash){
+    public static int sockMerchant(int[] arr,Map<Integer,Integer>hash){
         for(int i=0;i<arr.length;i++){
             int num=arr[i];
             if(hash.containsKey(num)){
                 hash.put(num,hash.get(num)+1);
-            }else{
+            }
+            else{
                 hash.put(num,1);
             }
         }
-        int max=0;
-        int result=0;
-        for(Integer key:hash.keySet()){
-            int value=hash.get(key);
-            if(value>max){
-                max=value;
-                result=key;
-            }
+        int count=0;
+        for(Integer key:hash.values()){
+                count+=key/2;
         }
-
-        return result;
+        return count;
     }
 }
