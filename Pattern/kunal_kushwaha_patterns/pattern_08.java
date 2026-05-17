324bc3ce9990d526baa1c3b06ee9edafcb07447b
Vimal Kumar <kvimal9560@gmail.com>
2026-05-18 01:16:09 +0530

    solved pattern_08

---
+++ Pattern/kunal_kushwaha_patterns/pattern_08.java
@@ -0,0 +1,19 @@
+package Pattern.kunal_kushwaha_patterns;
+import java.util.Scanner;
+public class pattern_08 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please Enter the row number ");
+        int n=sc.nextInt();
+        System.out.println("The number you have entered is " + n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<((i*2)+1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
