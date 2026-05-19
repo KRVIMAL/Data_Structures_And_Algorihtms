3bd5b5701251a8cd21911ac204daab84f109fcf8
Vimal Kumar <kvimal9560@gmail.com>
2026-05-20 01:15:00 +0530

    solved pattern_11

---
+++ Pattern/kunal_kushwaha_patterns/pattern_11.java
@@ -0,0 +1,19 @@
+package Pattern.kunal_kushwaha_patterns;
+import java.util.Scanner;
+public class pattern_11 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter the number of rows");
+        int n=sc.nextInt();
+        System.out.println("The number you have entered is " + n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<(n-i);k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
